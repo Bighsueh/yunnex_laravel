12480e75296a0df9cd50b0e454475a15924aac6b
Bighsueh <40841121@gm.nfu.edu.tw>
2021-06-14 06:28:15 +0800

    add front pages

---
+++ resources/views/pages/front/story.blade.php
@@ -0,0 +1,18 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>品牌故事</title>
+</head>
+<body>
+<div class="container-fluid">
+    <a href="{{route('story_front')}}">
+        <img style="width: 100%" src="{{asset('assets/images/front/story.JPG')}}">
+    </a>
+</div>
+
+</body>
+</html>
