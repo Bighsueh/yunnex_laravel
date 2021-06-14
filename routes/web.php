dbfb4134f2aa903c68f56c4548410c5a10c76ac2
Bighsueh <40841121@gm.nfu.edu.tw>
2021-06-14 13:59:05 +0800

    add router

--- routes/web.php
+++ routes/web.php
@@ -12,5 +12,22 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
-
+//home page
 Route::get('/', 'HomeController@menu')->name('home');
+
+//front pages
+Route::get('/intro_front', 'HomeController@intro_front')->name('intro_front');
+Route::get('/story_front', 'HomeController@story_front')->name('story_front');
+Route::get('/discount_front', 'HomeController@discount_front')->name('discount_front');
+Route::get('/sign_front', 'HomeController@sign_front')->name('sign_front');
+Route::get('/game_front', 'HomeController@game_front')->name('game_front');
+Route::get('/order_front', 'HomeController@order_front')->name('order_front');
+
+//content pages
+Route::get('/intro_content', 'HomeController@intro_content')->name('intro_content');
+Route::get('/story_content', 'HomeController@story_content')->name('story_content');
+Route::get('/story2_content', 'HomeController@story2_content')->name('story2_content');
+Route::get('/discount_content', 'HomeController@discount_content')->name('discount_content');
+Route::get('/sign_content', 'HomeController@sign_content')->name('sign_content');
+Route::get('/game_content', 'HomeController@game_content')->name('game_content');
+Route::get('/order_content', 'HomeController@order_content')->name('order_content');
