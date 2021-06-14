5e5c146feedf2e146566c6d1bbcb27454c48c58c
Bighsueh <40841121@gm.nfu.edu.tw>
2021-06-14 13:58:07 +0800

    add intro.blade.php

---
+++ resources/views/pages/contents/intro.blade.php
@@ -0,0 +1,92 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link href="{{asset('assects/css/bootstrap.css')}}" rel="stylesheet"/>
+    <title>產品介紹</title>
+</head>
+<body style="background-color: #DBEECE">
+<div class=""
+     style="background-color: #DBEECE;">
+    <div class="" style="display: inline-block;float: left;align-items: center">
+        <a href="{{route('home')}}"><img class="" style="height:80px;"
+                                         src="{{asset('assets/images/contents/rollback.png')}}"/></a>
+    </div>
+    <div class=""
+         style="display:inline-block ;align-items: center;margin: 2px;margin-left:30%;">
+        <h2 class="" style="color: #00357B;">產品介紹</h2>
+    </div>
+    <div class="" style="display: inline-block;float: right;align-items: center">
+        <img class="" style="height:80px;" src="{{asset('assets/images/yunnex_logo_transparent.png')}}"/>
+    </div>
+</div>
+<hr class="text-black" style="border-top: 3px solid black;width: 100%"/>
+<div class="container-fluid justify-content-center" style="display: inline-block">
+    <div class="col-12 row justify-content-around d-flex" style="min-width: 300px;">
+        <div class="col" style="float: left">
+            　　　　　
+            <a class="text-center"><img style="width: 100px;"
+                                        src="{{asset('assets/images/contents/kingkong_peanuts.png')}}"/></a>
+            <br>
+            <strong style="color: #00357B;">　　黑金剛花生仁(鹹酥、蔥蒜、麻辣)
+                <strong style="color: red">
+                    $85
+                </strong>
+            </strong>
+        </div>
+        　　　
+        <div class="col " style="float:right;  vertical-align: middle;">
+            <br>
+            <strong class="" style="vertical-align: middle;float: right">
+                採用在地黑金剛花生仁，三種不一樣風味，
+            </strong>
+            <br>
+            <strong class="" style="vertical-align: middle;float: right">
+                增加口感的層次，讓人一口接一口。
+            </strong>
+
+        </div>
+
+    </div>
+
+</div>
+<div class="container-fluid justify-content-center" style="display: inline-block">
+    <div class="col-12 row justify-content-around d-flex" style="min-width: 300px;">
+        <div class="col" style="float: left">
+            　　　　　
+            <a class="text-center"><img style="width: 100px;"
+                                        src="{{asset('assets/images/contents/elder_peanuts.png')}}"/></a>
+            <br>
+            <strong style="color: #00357B;">　　黑金剛花生仁(鹹酥、蔥蒜、麻辣)
+                <strong style="color: red">
+                    $180
+                </strong>
+            </strong>
+        </div>
+        　　　
+        <div class="col " style="float:right;  vertical-align: middle;">
+            <br>
+            <strong class="" style="vertical-align: middle;float: right">
+                採用在地黑金剛花生仁，三種不一樣風味，
+            </strong>
+            <br>
+            <strong class="" style="vertical-align: middle;float: right">
+                增加口感的層次，讓人一口接一口。
+            </strong>
+
+        </div>
+
+    </div>
+
+</div>
+<div class="" style="float: right;color: gray">
+    <strong >
+        (目前推這兩項商品，網站上還有除了花生的產品，歡迎大家購買~)
+    </strong>
+</div>
+
+</body>
+</html>
