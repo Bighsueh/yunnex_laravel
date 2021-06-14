534eae379dfd2408edcfefa59a271c451068f505
Bighsueh <40841121@gm.nfu.edu.tw>
2021-06-14 13:57:32 +0800

    add all function

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -10,4 +10,71 @@ class HomeController extends Controller
     {
         return view('pages.menu');
     }
+
+    public function intro_front()
+    {
+        return view('pages.front.intro');
+    }
+
+    public function story_front()
+    {
+        return view('pages.front.story');
+    }
+
+    public function discount_front()
+    {
+        return view('pages.front.discount');
+    }
+
+    public function sign_front()
+    {
+        return view('pages.front.sign');
+    }
+
+    public function game_front()
+    {
+        return view('pages.front.game');
+    }
+
+    public function order_front()
+    {
+        return view('pages.front.order');
+    }
+
+    public function intro_content()
+    {
+        return view('pages.contents.intro');
+    }
+
+    public function story_content()
+    {
+        return view('pages.contents.story');
+    }
+
+    public function story2_content()
+    {
+        return view('pages.contents.story2');
+    }
+
+    public function discount_content()
+    {
+        return view('pages.contents.discount');
+    }
+
+    public function sign_content()
+    {
+        return view('pages.contents.sign');
+    }
+
+    public function game_content()
+    {
+        return view('pages.contents.game');
+    }
+
+    public function order_content()
+    {
+        return view('pages.contents.order');
+    }
+
+
 }
