b36d364aba4e68497cdc54393245e7d08a52585c
Bighsueh <40841121@gm.nfu.edu.tw>
2021-06-16 08:00:54 +0800

    add 折價券使用判斷

--- resources/views/pages/contents/discount.blade.php
+++ resources/views/pages/contents/discount.blade.php
@@ -47,19 +47,31 @@
         </h4>
     </div>
     <div class= "text-nowrap col-12 row container text-center justify-content-center" >
-            <img class="center-block d-block text-left" style="display: inline-block; width: 20%"
+            <img onclick="get_discount('10_percent_off')" id="10_percent_off" class="center-block d-block text-left" style="display: inline-block; width: 20%"
                  src="{{asset('assets/images/contents/10_percent_off.png')}}"/>
-            <img class="center-block d-block text-center" style="display: inline-block; width: 20%"
+            <img onclick="get_discount('twice85percent')" id="twice85percent" class="center-block d-block text-center" style="display: inline-block; width: 20%"
                  src="{{asset('assets/images/contents/twice85percent.png')}}"/>
-            <img class="center-block d-block text-right" style="display: inline-block; width: 20%"
+            <img onclick="get_discount('50dollor_discount')" id="50dollor_discount" class="center-block d-block text-right" style="display: inline-block; width: 20%"
                  src="{{asset('assets/images/contents/50dollor_discount.png')}}"/>
-            <img class="center-block d-block text-right" style="display: inline-block; width: 20%"
+            <img onclick="get_discount('next_time')" id="next_time" class="center-block d-block text-right" style="display: inline-block; width: 20%"
                  src="{{asset('assets/images/contents/next_time.png')}}"/>
-
     </div>
-
-
 </div>
+<script>
+    var discount_check = false;
+    function get_discount(img_name) {
+        if(!discount_check){
+            if(window.confirm('您確定要使用折價券嗎? \n *提醒您每日只能使用一張折價券')){
+                document.getElementById(img_name).style.opacity = "0.2";
+                discount_check = true;
+            }
+        }else{
+            window.alert('您今日已到達折價券使用上限!!');
+        }
 
+    }
+
+
+</script>
 </body>
 </html>
